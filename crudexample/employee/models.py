ac3058f85659919391e32ec8493c5c0101ab1535
Antonio Takev <tonitakev@gmail.com>
2019-05-30 21:07:22 +0300

    project

---
+++ crudexample/employee/models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+    
+class Employee(models.Model):
+    eid = models.CharField(max_length=20)
+    ename = models.CharField(max_length=100)
+    eemail = models.EmailField()
+    econtact = models.CharField(max_length=15)
+    class Meta:
+        db_table = "employee"
---
+++ crudexample/employee/urls.py
@@ -0,0 +1,26 @@
+"""crudexample URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from employee import views
+
+urlpatterns = [
+    url(r'^emp/$', views.emp),
+    url(r'^show/$',views.show),
+    url(r'^edit/<int:id>', views.edit),
+    url(r'^update/<int:id>', views.update),
+    url(r'^delete/<int:id>', views.destroy),
+]
